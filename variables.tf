2d1d9695efe506fb36e3a05f180636118dc721f5
Larry Eichenbaum <6776768+larryebaum@users.noreply.github.com>
2020-12-01 15:18:07 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,6 +5,51 @@
 # Terraform code. If you create a variable with no default, the user will be
 # prompted to enter it (or define it via config file or command line flags.)
 
+variable "name" {
+  description = "name to pass to name tag"
+  default = "Larry Eichenbaum"
+}
+
+variable "owner" {
+  description = "name to pass to owner tag"
+  default = "larryebaum"
+}
+
+variable "ttl" {
+  description = "ttl to pass to ttl tag"
+  default = "4"
+}
+
+variable "se_region" {
+  description = "SE region assigned"
+  default = "public-sector"
+}
+
+variable "purpose" {
+  description = "Purpose; Required if TTL = -1"
+  default = "demo"
+}
+
+variable "terraform" {
+  description = "Built by Terraform"
+  default = true
+}
+
+variable "customer" {
+  description = "Billable Customer"
+  default = ""
+}
+
+variable "tfe_workspace" {
+  description = "TFE Workspace"
+  default = ""
+}
+
+variable "lifecycle_action" {
+  description = "stop or terminate (default)"
+  default = "terminate"
+}
+
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
 }
